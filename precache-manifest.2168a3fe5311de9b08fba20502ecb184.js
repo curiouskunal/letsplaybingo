c8e2005be39ca587a381e394a84a5882799666af
Kunal Shah <kunal8@gmail.com>
2019-11-10 21:01:28 -0500

    Updates

---
+++ precache-manifest.2168a3fe5311de9b08fba20502ecb184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ed79cb401f2b0627027976a756af92",
+    "url": "/letsplaybingo/index.html"
+  },
+  {
+    "revision": "a4bab6aa2cd1eeaa5d95",
+    "url": "/letsplaybingo/static/css/2.699efa4f.chunk.css"
+  },
+  {
+    "revision": "5c2e6bfa954c3c00d6dd",
+    "url": "/letsplaybingo/static/css/main.f29ea1a6.chunk.css"
+  },
+  {
+    "revision": "a4bab6aa2cd1eeaa5d95",
+    "url": "/letsplaybingo/static/js/2.c831cc3b.chunk.js"
+  },
+  {
+    "revision": "5c2e6bfa954c3c00d6dd",
+    "url": "/letsplaybingo/static/js/main.06ab1973.chunk.js"
+  },
+  {
+    "revision": "3415b3e29ca13ae70888",
+    "url": "/letsplaybingo/static/js/runtime-main.5cb44c3a.js"
+  },
+  {
+    "revision": "5a6f7124fc364ee38beca9cf869cec60",
+    "url": "/letsplaybingo/static/media/logo.5a6f7124.svg"
+  }
+]);
